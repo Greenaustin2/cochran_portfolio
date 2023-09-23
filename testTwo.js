f5f18fb040005753617f722db0fabb629f367fa7
Greenaustin2 <greenaustin2@gmail.com>
2023-09-23 13:26:27 -0400

    random infinite scroll finalized

--- testTwo.js
+++ testTwo.js
@@ -1,11 +1,27 @@
 const container = document.querySelectorAll(".container");
 
-const imagesTop = [...Array(9)].map(
-  (x, i) => "../images/top/0" + (i + 1) + ".jpg"
-);
-const imagesBottom = [...Array(9)].map(
-  (x, i) => "../images/bottom/0" + (i + 1) + ".jpg"
-);
+function randomImageArray(divIdentifier) {
+  var randoms = new Set();
+  while (randoms.size < 9) {
+    randoms.add(1 + Math.floor(Math.random() * 9));
+  }
+
+  let randomArray = Array.from(randoms);
+  var finalArray = randomArray.map(
+    (x, i) => "../images/" + divIdentifier + "/0" + x + ".jpg"
+  );
+  return finalArray;
+}
+
+const imagesTop = randomImageArray("top");
+const imagesBottom = randomImageArray("bottom");
+
+//appends and prepends repeat elements for the infinite gallery illusion loop points
+imagesBottom.push(imagesBottom[0], imagesBottom[1]);
+imagesBottom.unshift(imagesBottom[7], imagesBottom[8]);
+imagesTop.push(imagesTop[0], imagesTop[1]);
+imagesTop.unshift(imagesTop[7], imagesTop[8]);
+
 const imageArrays = [imagesTop, imagesBottom];
 
 const varToString = (varObj) => Object.keys(varObj)[0];
@@ -13,49 +29,40 @@ const varToString = (varObj) => Object.keys(varObj)[0];
 function loadImages(container, imageArrays) {
   var x = 0;
   container.forEach((item) => {
-    var randoms = new Set();
-    while (randoms.size < 9) {
-      randoms.add(1 + Math.floor(Math.random() * 9));
-    }
-    randoms.forEach((i) => {
-      console.log(i);
+    imageArrays[x].forEach((i) => {
+      //randomly appends images from list to dom
       const img = document.createElement("img");
-      img.className = "child bg-" + i;
-      img.src = imageArrays[x][i - 1];
+      img.className = "child";
+      img.src = i;
       item.appendChild(img);
       i++;
     });
     x += 1;
-    console.log("x" + x);
   });
 }
 
+function appendPrependImages(container, imageArrays) {}
+
 let top = document.querySelector(".top");
-console.log(top);
 let bottom = document.querySelector(".bottom");
 
 //infinite-snap-scroll
 const snapScroll = (targetElement) => {
-  //   console.log(targetElement.className.split(" ")[2]);
   let children = targetElement.querySelectorAll(".child");
   let num, slideNum;
 
   // This will log the width of the viewport
   num = window.innerWidth;
-
   slideNum = num * (children.length - 1);
-  //   console.log(children.length);
 
   //scroll forward
   targetElement.addEventListener("scroll", function () {
     if (targetElement.scrollLeft == slideNum) {
-      targetElement.scrollTo(num, 0);
-      console.log("left");
+      targetElement.scrollTo(num * 3, 0);
     }
     //scroll backward
     if (targetElement.scrollLeft == 0) {
-      targetElement.scrollTo(num * 8, 0);
-      console.log("right");
+      targetElement.scrollTo(num * 9, 0);
     }
   });
   // in case of resize
